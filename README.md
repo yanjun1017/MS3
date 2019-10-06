3323dead19361d205c8e07a456da941d48bc59af
yanjun1017 <yanjunli1017@gmail.com>
2019-10-06 12:28:10 -0400

    questions and write-ups

---
+++ README.md
@@ -0,0 +1,89 @@
+# **MS3**
+This application is used to parse the data entries from ms3Interview.csv and insert the valid data to the database table, which is created inside the application. And, the application writes the invalid data entries to the ms3interview-bad.csv files. At the end of the processing, a log file is created which contains statistics of the data entries. 
+####
+
+## **Getting Started**
+The instructions will get you a copy of the project up and running on your local machine for deployment and testing purposes. 
+### **Prerequisites**
+What things you need to install
+```
+JDBC driver 
+SQLite database system
+Java IDE
+``` 
+### **Running the application**
+What things you need to run on your terminal
+
+Add JDBC driver to the project path
+```
+export CLASSPATH=/path/mysql-connector-java-ver-bin.jar:$CLASSPATH
+```
+Compile the project
+```
+javac Reader.java
+```
+Run the project
+```
+java Reader
+```
+
+## **Questions**
+###**Question 1**
+```
+This problem is implemented with 4 functions.
+``` 
+####createDatabase()
+```
+This function is intended to create a SQLite database called ms3Interview.db with the JDBC driver. 
+    - Parameters: None
+    - Return: None
+    - Throws:
+        * SQLException if a SQL error occurs.
+        * ClassNotFoundException if a class not found. 
+```
+    
+####createTable()
+```
+This function is intended to create a table called ms3InterviewTB in the existing database ms3Interview.db with columns (A, B, C, D, E, F, G, H, I, J). 
+    - Parameters: None
+    - Return None
+    - Throws:
+        * SQLException if a SQL error occurs.
+        * ClassNotFoundException if a class not found. 
+```
+
+####connect()
+```
+This function is used as a helper function for function, insert(). It is used to connect to the database, ms3Interview.db. 
+    - Parameters: None
+    - Return: the Connection object
+    - Throws:
+      * SQLException if a SQL error occurs.
+```
+
+####insert(String A, String B, String C, String D, String E, String F, String G, String H, String I, String J)
+```
+This function is intended to insert the values (A, B, C, D, E, F, G, H, I, J) and insert the values to the corresponding columns of the existing table, ms3InterviewTB, in the database ms3Interview.db. 
+    - Parameters: 10 Strings which represents the values of the specific column. 
+    - Return: None
+    - Throws:
+         * SQLException if a SQL error occurs.
+```
+###**Question 2**
+```
+This is implemented inside the main function. 
+For all data entries that does not meet the if statement, ((datas.length == 10) && datas[9] != null), the application will use the PrintWriter to store the bad data entries to the ms3interview-bad.csv file. 
+```
+###**Question 3**
+```
+This problem is implemented with 1 function.
+```
+####public static void log(int recordCount, int successCount, int failCount)
+```
+This function is used to create a log file and write statistics to the log file.
+    - Parameters: recordCount is the number of records data entries read. successCount is the number of successful data read, which means the specific data entry should contain only 10 valid columns. failCount is the number of unccessful data entries read.
+    - Return: None
+    - Throws:
+        * IOException if a I/O error occurs.
+    
+```
--- reader.java
+++ reader.java
@@ -1,16 +1,5 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.DriverManager;
-import java.sql.Statement;
-import java.sql.SQLException;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.*;
+import java.sql.*;
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;
 import java.util.logging.Level;
@@ -19,6 +8,73 @@ import java.util.logging.Level;
 public class Reader {
 
     private static final String url = "jdbc:sqlite:./sqlite/ms3Interview.db";
+
+    public static void main(String[] args) {
+
+        String csvFile = "./source/ms3Interview.csv";
+        BufferedReader buffer = null;
+        String line = "";
+        // set recoundCount to -1 in order to eliminate the header line
+        int recordCount = -1;
+        int successCount = 0;
+        int failCount = 0;
+
+        createDatabase();
+        createTable();
+
+        try(PrintWriter pw = new PrintWriter(new File("./source/ms3Interview-bad.csv"))){
+
+            buffer = new BufferedReader(new FileReader(csvFile));
+
+            // buffer reads each data entry line by line
+            while ((line = buffer.readLine()) != null) {
+
+                if (recordCount == -1){
+                    recordCount += 1;
+                }
+                else {
+                    recordCount += 1;
+                    String[] datas = line.split(",");
+
+                    // the data entry is consider as successful if and only if
+                    // each data entry includes at least 9 data sets
+                    // and datas[9] should be accessible
+                    if ((datas.length == 10) && datas[9] != null){
+
+                        insert(datas[0], datas[1], datas[2], datas[3], datas[4], datas[5], datas[6], datas[7], datas[8], datas[9]);
+                        successCount += 1;
+                    }
+                    // bad data entries write to the bad csv file
+                    else {
+
+                        pw.println(line);
+                        failCount += 1;
+                    }
+                }
+            }
+
+            // Write the log file
+            log(recordCount, successCount, failCount);
+            // Close the printwriter after finished writing the csv file
+            pw.close();
+
+            // Exceptions
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        } finally {
+            if (buffer != null) {
+                try {
+                    buffer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 
     public static void createDatabase(){
 
@@ -29,9 +85,9 @@ public class Reader {
             if (conn != null){
                 DatabaseMetaData meta = conn.getMetaData();
             }
-        } catch(SQLException e){
+        } catch (SQLException e){
             e.printStackTrace();
-        } catch(ClassNotFoundException e) {
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
@@ -39,7 +95,7 @@ public class Reader {
     public static void createTable(){
 
 
-        String sql = "CREATE TABLE IF NOT EXISTS ms3InterviewDB (\n"
+        String sql = "CREATE TABLE IF NOT EXISTS ms3InterviewTB (\n"
                 + "  A  String,\n"
                 + "  B  String,\n"
                 + "  C  String,\n"
@@ -54,6 +110,7 @@ public class Reader {
         Connection conn = null;
 
         try {
+
             Class.forName("org.sqlite.JDBC");
             conn = DriverManager.getConnection(url);
 
@@ -62,7 +119,7 @@ public class Reader {
 
         } catch (SQLException e) {
             e.printStackTrace();
-        } catch (Exception e){
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
@@ -80,7 +137,7 @@ public class Reader {
 
     public static void insert(String A, String B, String C, String D, String E, String F, String G, String H, String I, String J) {
 
-        String sql = "INSERT INTO ms3InterviewDB(A, B, C, D, E, F, G, H, I, J) VALUES(?,?,?,?,?,?,?,?,?,?)";
+        String sql = "INSERT INTO ms3InterviewTB(A, B, C, D, E, F, G, H, I, J) VALUES(?,?,?,?,?,?,?,?,?,?)";
 
         try (Connection conn = connect();
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
@@ -100,107 +157,22 @@ public class Reader {
         }
     }
 
-    public static void main(String[] args) {
+    public static void log(int recordCount, int successCount, int failCount){
 
-        String csvFile = "./source/ms3Interview.csv";
-        BufferedReader buffer = null;
-        String line = "";
-        // set recoundCount to -1 in order to eliminate the header line
-        int recordCount = -1;
-        int sucessCount = 0;
-        int failCount = 0;
-
-        createDatabase();
-        createTable();
-
-        try(PrintWriter pw = new PrintWriter(new File("./source/ms3Interview-bad.csv"))){
-
-            // logger to write to the log file
+        // logger to write to the log file
+        try{
             Logger logger = Logger.getLogger("./source/ms3Interview.log");
             FileHandler fh = new FileHandler("./source/ms3Interview.log", true);
             logger.addHandler(fh);
 
-            buffer = new BufferedReader(new FileReader(csvFile));
-
-            // buffer reads each data entry line by line
-            while ((line = buffer.readLine()) != null) {
-
-                if (recordCount == -1){
-                    recordCount += 1;
-                }
-                else {
-                    recordCount += 1;
-                    String[] datas = line.split(",");
-
-                    // the data entry is consider as successful if and only if
-                    // each data entry includes at least 9 data sets
-                    // and datas[9] should be accessible
-                    if (datas.length > 9 && datas[9] != null) {
-
-
-                        String A = datas[0];
-                        String B = datas[1];
-                        String C = datas[2];
-                        String D = datas[3];
-                        String E = datas[4];
-                        String F = datas[5];
-                        String G = datas[6];
-                        String H = datas[7];
-                        String I = datas[8];
-                        String J = datas[9];
-
-//                        String sql = "INSERT INTO ms3InterviewDB(A, B, C, D, E, F, G, H, I, J) VALUES(" + A + "," + B + ",'" + C + "'," + D + "," + E + "," + F + ",'" + G + "'," + H + "," + I + "," + J + ")";
-//                        try (Connection conn = connect()){
-//                             Statement stmt = conn.createStatement();
-//                             stmt.execute(sql);
-//                        } catch (SQLException e) {
-//                            e.printStackTrace();
-//                        }
-                        insert(A, B, C, D, E, F, G, H, I, J);
-//                        String sql = "INSERT INTO ms3InterviewDB(A, B, C, D, E, F, G, H, I, J) VALUES(?,?,?,?,?,?,?,?,?,?)";
-//                        try (Connection conn = this.connect();
-//                             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-//                            pstmt.setString(1, name);
-//                            pstmt.setDouble(2, capacity);
-//                            pstmt.executeUpdate();
-//                        } catch (SQLException e) {
-//                            System.out.println(e.getMessage());
-//                        }
-                        sucessCount += 1;
-                    }
-                    // bad data entry should write to the bad csv file
-                    else {
-
-                        pw.println(line);
-                        failCount += 1;
-                    }
-                }
-            }
             if (logger.isLoggable(Level.INFO)){
                 // input the log message
                 logger.info(recordCount + " of records received\n");
-                logger.info(sucessCount + " of records successful\n");
+                logger.info(successCount + " of records successful\n");
                 logger.info(failCount + " of records failed\n");
             }
-
-            // Close the printwriter after finished writing the csv file
-            pw.close();
-
-            // Exceptions
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        } catch (SecurityException e) {
-            e.printStackTrace();
-        } finally {
-            if (buffer != null) {
-                try {
-                    buffer.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
         }
     }
 }
